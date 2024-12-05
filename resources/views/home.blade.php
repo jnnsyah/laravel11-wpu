f5b9fd0914c911f0297727ff807a8fd1f160de72
jiansyah <jiansyah1248@gmail.com>
2024-12-05 11:08:35 +0700

    Membuat layout, ,  kirim data trima data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
-    <title>Home Page</title>
-</head>
-<body class="h-full">
-
-<div class="min-h-full" >
-    <x-nav></x-nav>
-
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <!-- Your content -->
-    </div>
-  </main>
-</div>
-
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+</x-layout>
