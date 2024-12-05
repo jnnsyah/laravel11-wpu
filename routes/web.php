ada007e0be141624f9ff43be3c41841781696824
jiansyah <jiansyah1248@gmail.com>
2024-12-05 17:59:25 +0700

    Membuat model untuk post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Posts;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -12,22 +13,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog', ['title'=> 'Blog', 'posts' => [
-        [
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Rama Jiansyah',
-            'date' => '1 Januari 2024',
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem minus. Consequatur ipsa veniam asperiores error inventore excepturi vero dolores. Molestias corrupti consequatur, natus dolore aliquid sed odio sapiente unde!"
-        ],
-        [
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Rama Jiansyah',
-            'date' => '1 Februari 2024',
-            'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem minus. Consequatur ipsa veniam asperiores error inventore excepturi vero dolores. Molestias corrupti consequatur, natus dolore aliquid sed odio sapiente unde!"
-        ]
-    ]]);
+    return view('blog', data: ['title'=> 'Blog', 'posts' => posts::getAll()]);
 });
 
 Route::get('/contact', function () {
@@ -41,26 +27,6 @@ Route::get('/contact', function () {
 
 //wildcard -> menangkap apapun yang ada di url 
 Route::get('/posts/{slug}', function ($slug) {
-   $posts = [
-    [
-        'slug' => 'judul-artikel-1',
-        'title' => 'Judul Artikel 1',
-        'author' => 'Rama Jiansyah',
-        'date' => '1 Januari 2024',
-        'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem minus. Consequatur ipsa veniam asperiores error inventore excepturi vero dolores. Molestias corrupti consequatur, natus dolore aliquid sed odio sapiente unde!"
-    ],
-    [
-        'slug' => 'judul-artikel-2',
-        'title' => 'Judul Artikel 2',
-        'author' => 'Rama Jiansyah',
-        'date' => '1 Februari 2024',
-        'body' => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quidem minus. Consequatur ipsa veniam asperiores error inventore excepturi vero dolores. Molestias corrupti consequatur, natus dolore aliquid sed odio sapiente unde!"
-    ]
-    ];
-
-    $post = Arr::first($posts, function($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
-
+    $post = posts::getSingle($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
