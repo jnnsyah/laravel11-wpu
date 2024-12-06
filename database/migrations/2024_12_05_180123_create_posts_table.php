f741910f8776f1fb29964e0bac8643e8198b67a6
jiansyah <jiansyah1248@gmail.com>
2024-12-06 08:24:29 +0700

    Benerin migration biar bisa input data lewat tinker

--- database/migrations/2024_12_05_180123_create_posts_table.php
+++ database/migrations/2024_12_05_180123_create_posts_table.php
@@ -16,8 +16,9 @@ return new class extends Migration
             $table->string('slug');
             $table->string('title');
             $table->string('author');
-            $table->string('Date');
+            $table->string('Date')->nullable();
             $table->text('body');
+            $table->timestamps();
         });
     }
 
